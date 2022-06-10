165fa38ff98af1a41c8f15dd51af4a3027ac9eeb
Jake Dunafon <jlad795@gmail.com>
2022-06-10 14:05:24 -0700

    Remove content from article cards

--- src/ArticleCard/ArticleCard.js
+++ src/ArticleCard/ArticleCard.js
@@ -3,14 +3,16 @@ import './ArticleCard.css'
 
 const ArticleCard = ({article}) => {
     const image = article.multimedia ? <img src={article.multimedia[0].url} /> : <h1>No Image</h1>
-    return (
-        <article className='article-card'>
-            <h2>{article.title}</h2>
-            {image}
-            <h3>{article.abstract}</h3>
-            <p>{article.updated_date}</p>
-        </article>
-    )
+
+    if(article.title){
+        return (
+            <article className='article-card'>
+                <h2>{article.title}</h2>
+                {image}
+            </article>
+        )
+    }
+    
 }
 
 export default ArticleCard
