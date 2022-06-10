018f6900acd70a1da359143d1dfd1a054aa63f07
Jake Dunafon <jlad795@gmail.com>
2022-06-10 14:50:52 -0700

    Add content to view one

--- src/ViewOne/ViewOne.js
+++ src/ViewOne/ViewOne.js
@@ -11,7 +11,10 @@ const ViewOne = (props) => {
         <section className='view-one'>
             <h1>{oneArticle.title}</h1>
             {image}
+            <p>{oneArticle.multimedia[0].caption}</p>
             <h2>{oneArticle.abstract}</h2>
+            <a href={oneArticle.url}>View Full Article</a>
+            <h3>Published: {oneArticle.published_date}</h3>
         </section>
     )
 }
