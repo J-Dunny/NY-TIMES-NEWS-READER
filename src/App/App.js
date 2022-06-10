b63da20f712ae2fccc31d50c2ff7c4fc36137216
Jake Dunafon <jlad795@gmail.com>
2022-06-10 13:50:08 -0700

    Add homePage

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,21 @@
 import getArticles from '../apiCalls';
 import './App.css';
 import { useEffect, useState } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import HomePage from '../HomePage/HomePage';
 function App() {
+const [allArticles, setAllArticles] = useState([])
 
 useEffect(() => {
-getArticles().then(data => console.log(data))
+getArticles().then(data => setAllArticles(data.results))
 }, [])
 
+console.log(allArticles)
   return (
-    <div>
-      <h1>hi</h1>
+    <div className='app'>
+      <Routes>
+        <Route path='/' element={<HomePage allArticles={allArticles} />}/>
+      </Routes>
     </div>
   );
 }
