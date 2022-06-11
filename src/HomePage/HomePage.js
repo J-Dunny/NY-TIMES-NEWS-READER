b9fead6070a9ef08c9255cada5a2c90a3f4f922e
Jake Dunafon <jlad795@gmail.com>
2022-06-11 09:38:45 -0700

    Remove nav

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -8,9 +8,6 @@ const HomePage = (props) => {
         const titles = props.allArticles.map(article => <ArticleCard article={article} key={article.title}/>)
         return (
             <section className='homepage'>
-                <nav>
-                    <h1>NY Times Science</h1>
-                </nav>
                 <section className='all-articles'>
                 {titles}
                 </section>
