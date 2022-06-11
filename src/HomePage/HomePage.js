f34ad54f719da5ca217c8c9a83c612b1d2b9603a
Jake Dunafon <jlad795@gmail.com>
2022-06-11 10:07:34 -0700

    Add search

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,15 +1,25 @@
 import './HomePage.css'
 import ArticleCard from '../ArticleCard/ArticleCard'
+import { useState } from 'react'
 
 const HomePage = (props) => {
+const [search, setSearch] = useState('')
 
     if (props.allArticles){
+        const filtered = props.allArticles.filter(article => article.title.includes(search) || article.title.toLowerCase().includes(search))
+        const articles = filtered.map(article => <ArticleCard article={article} key={article.title}/>)
         
-        const titles = props.allArticles.map(article => <ArticleCard article={article} key={article.title}/>)
         return (
             <section className='homepage'>
+                <input 
+                type='text' 
+                className='search'
+                value={search}
+                placeholder='Search'
+                onChange={e => setSearch(e.target.value)}
+                />
                 <section className='all-articles'>
-                {titles}
+                {articles}
                 </section>
             </section>
         )
